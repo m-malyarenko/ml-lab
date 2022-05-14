5e8ac0233b7e35567dd14e45ca36628b1418368d
Mikhail Malyarenko <malyarenko.md@gmail.com>
2022-05-14 19:21:37 +0300

    fex: rusult output & plot fixed

--- 4_tree/spam.R
+++ 4_tree/spam.R
@@ -21,13 +21,14 @@ tree <- tree(
 plot(tree, type = "uniform")
 text(tree, cex = 0.7)
 
-result_colnames <- c("k", "Accuracy")
+result_colnames <- c("k", "Accuracy", "Splits")
 result_table <- data.frame(matrix(nrow = 0, ncol = length(result_colnames)))
 colnames(result_table) = result_colnames
 
 # Prune tree to sequence of trees
 prune_tree_seq <- prune.tree(tree, method = "misclass")
 prune_k_seq <- prune_tree_seq$k[-1]
+n_k <- length(prune_k_seq)
 
 accuracy <- c()
 
@@ -41,14 +42,45 @@ for (k in prune_k_seq) {
   current_accuracy <- sum(pred == data_test$yesno) / n_test * 100
   accuracy <- append(accuracy, current_accuracy)
   
-  result_row <- data.frame(k = k, Accuracy = current_accuracy)
+  result_row <- data.frame(
+    k = k,
+    Accuracy = current_accuracy,
+    Splits = nrow(prune_tree$frame)
+  )
   result_table <- rbind(result_table, result_row)
 }
 
 print(result_table)
+write.csv(result_table, "data/spam_tree_k_stat.csv", quote = FALSE, row.names = FALSE)
+
+result_table$Splits <- scale(result_table$Splits)
+result_table$Accuracy <- scale(result_table$Accuracy)
+
 
 # Pick optimal k
-plot(x = prune_k_seq, y = accuracy, type = "b", pch = 20)
+plot(
+  x = prune_k_seq[1:(n_k - 2)],
+  y = result_table$Splits[1:(n_k - 2)],
+  type = "b",
+  pch = 20,
+  col = "red",
+  
+  xlab = "k",
+  ylab = ""
+)
+points(
+  x = prune_k_seq[1:(n_k - 2)],
+  y = result_table$Accuracy[1:(n_k - 2)],
+  type = "b",
+  pch = 20,
+  col = "green"
+)
+legend(
+  x = "topright",
+  legend = c("Точность", "Количество узлов"),
+  fill = c("green", "red")
+)
+
 best_accuracy <- prune.tree(tree, prune_k_seq[which.max(accuracy)])
 accuracy_size_tradeoff <- prune.tree(tree, prune_k_seq[18])
 
